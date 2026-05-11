46ddf46e6a0f450884f6235ec17eb99d7b516a06
Micha Schirmer <michajava8@gmail.com>
2026-05-11 09:16:36 +0200

    Kommentar hinzugefügt

--- ContactsFX_v02/src/main/java/com/example/contacts/Launcher.java
+++ ContactsFX_v02/src/main/java/com/example/contacts/Launcher.java
@@ -2,7 +2,11 @@ package com.example.contacts;
 
 import javafx.application.Application;
 
+/**
+ * start App
+ */
 public class Launcher {
+
     public static void main(String[] args) {
         Application.launch(ApplicationView.class, args);
 
